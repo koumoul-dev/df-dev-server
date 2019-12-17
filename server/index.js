3256e1ddaef938b3b6168fe0f3e8d940b292a1ac
Alban Mouton <alban.mouton@gmail.com>
2019-12-17 22:48:39 +0100

    chore: blue link to dev server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk')
 const path = require('path')
 process.env.NODE_CONFIG_DIR = path.join(__dirname, '../config')
 const config = require('config')
 const app = require('./app')
 
 app.run().then(app => {
-  console.log('DataFair dev server listening on http://localhost:%s', config.port)
+  console.log(chalk.blue('\nDataFair dev server available on ') + chalk.underline.bold.blue(`http://localhost:${config.port}\n`))
 }, error => {
   console.error('Failure in customers process', error)
   process.exit(-1)
