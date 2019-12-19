37a73c6d362ecd0d2710be0d627b8835907bbf65
Alban Mouton <alban.mouton@gmail.com>
2019-12-19 14:53:57 +0100

    fix: typo

--- public/pages/index.vue
+++ public/pages/index.vue
@@ -34,7 +34,7 @@
           </v-btn>
         </v-row>
         <v-card>
-          <v-iframe v-if="showPreview" src="http://localhost:5888/app?draft=rue" />
+          <v-iframe v-if="showPreview" src="http://localhost:5888/app?draft=true" />
         </v-card>
       </v-col>
     </v-row>
